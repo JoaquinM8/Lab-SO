db8ae8ff0c229e7f7eab62ca32d7a3681c1f25e1
JoaquinM8 <joaquiloquito@gmail.com>
2025-05-26 10:47:40 -0300

    5-26

--- 25-5-26/6.py
+++ 25-5-26/6.py
@@ -2,3 +2,16 @@ tr = 0
 md = 0
 c = 0
 
+while True:
+    d = int(input("Ingresar donación > "))
+    if d == 0:
+        break
+    else:
+        if d > md:
+            md = d
+        tr += d
+        c += 1
+
+print("Donadores:",c)
+print("Total donado:",tr)
+print("Mayor doncaión:",md)
