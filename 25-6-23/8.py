6636602e34b6335212367ef406ee0e517210c8c1
JoaquiPy <joaquiloquito@gmail.com>
2025-06-29 20:51:05 -0300

    6-23

---
+++ 25-6-23/8.py
@@ -0,0 +1,23 @@
+Vec = []
+tamVec = int( input("Ingrese dimensión del vector: "))
+pares = tamVec*[0]
+impares = tamVec*[0]
+
+print ( "Ingrese los", tamVec, "valores del vector")
+for x in range(tamVec):
+    elemento = int( input("Elemento {}: ".format(x+1)))
+    Vec.append(elemento)
+    
+vpr = 0
+i = 0
+for x in range(tamVec):
+    if Vec[x] % 2 == 0:
+        pares[vpr] = Vec[x]
+        vpr = vpr + 1
+    else:
+        impares[i] = Vec[x]
+        i = i + 1
+print(pares[0:vpr])
+print ( "El vector de pares tiene" , vpr, "elementos")
+print(impares[0:i])
+print ( "El vector de impares tiene",i,"elementos")
